6c98820a2857231106ff0d41d8f1966f33931630
Angkasit Karoonchart <108455691+Marguro@users.noreply.github.com>
2025-07-01 13:39:59 +0700

    update: enemy logic
    also refactor enemy scripts

--- Assets/Kawaii Survivor/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/Enemy/EnemyMovement.cs
@@ -4,37 +4,23 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [Header(" Elements ")] 
+    [Header(" Elements ")]
     private Player player;
     
     [Header(" Settings ")] 
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float playerDetectionRadius;
-    
-    [Header(" Debug ")]
-    [SerializeField] private bool gizmos;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        player = FindAnyObjectByType<Player>();
-
-        if (player == null)
-        {
-            Debug.LogWarning("No player found");
-            Destroy(gameObject);
-        }
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        FollowPlayer();
-
-        TryAttack();
+        if (player != null) 
+            FollowPlayer();
     }
 
+    public void StorePlayer(Player player)
+    {
+        this.player = player;
+    }
+    
     private void FollowPlayer()
     {
         Vector2 direction = (player.transform.position - transform.position).normalized;
@@ -42,23 +28,5 @@ public class EnemyMovement : MonoBehaviour
         Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
 
         transform.position = targetPosition;
-    }
-
-    private void TryAttack()
-    {
-        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-
-        if (distanceToPlayer <= playerDetectionRadius)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        if (!gizmos) return;
-        
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
